839064923d44e391b639beab3272e76ea010ef06
MrGeislinger <9027783+MrGeislinger@users.noreply.github.com>
2023-12-13 21:58:44 -0800

    Make sure new chat gets saved after first message

--- st_chat.py
+++ st_chat.py
@@ -8,17 +8,27 @@ GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
 new_chat_id = f'{time.time()}'
+model_role = 'ai'
 
 # Sidebar allows a list of past chats
 with st.sidebar:
     st.write('# Past Chats')
     past_chats: dict = joblib.load('past_chats.json')
-    chat_id = st.selectbox(
-        label='Pick a past chat',
-        options=[new_chat_id] + list(past_chats.keys()),
-        format_func=lambda x: past_chats.get(x, 'New Chat'),
-        placeholder='_',
-    )
+    if st.session_state.get('chat_id') is None:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id] + list(past_chats.keys()),
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='_',
+        )
+    else:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
+            index=1,
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='_',
+        )
     # Save new chats after a message has been sent to AI
         
 
@@ -26,8 +36,12 @@ st.write('# Chat with AI')
 
 # Chat history (allows to ask multiple questions)
 try:
-    st.session_state.messages = joblib.load(f'data/{chat_id}-st_messages')
-    st.session_state.gemini_history = joblib.load(f'data/{chat_id}-gemini_messages')
+    st.session_state.messages = joblib.load(
+        f'data/{st.session_state.chat_id}-st_messages'
+    )
+    st.session_state.gemini_history = joblib.load(
+        f'data/{st.session_state.chat_id}-gemini_messages'
+    )
     print('old cache')
 except:
     st.session_state.messages = []
@@ -47,10 +61,12 @@ for message in st.session_state.messages:
 # React to user input
 if prompt := st.chat_input('Your message here...'):
     # Save this as a chat for later
-    if chat_id not in past_chats.keys():
+    if st.session_state.chat_id not in past_chats.keys():
         # TODO: Give user a chance to name chat
         chat_title = f'NEW'
-        past_chats[chat_id] = f'{chat_title}-{chat_id}'
+        past_chats[st.session_state.chat_id] = (
+            f'{chat_title}-{st.session_state.chat_id}'
+        )
     joblib.dump(past_chats, 'past_chats.json')
     # Display user message in chat message container
     with st.chat_message('user'):
@@ -70,8 +86,8 @@ if prompt := st.chat_input('Your message here...'):
     )
     # Display assistant response in chat message container
     with st.chat_message(
-        name='ai',
-        # avatar='✨',
+        name=model_role,
+        avatar='✨',
     ):
         message_placeholder = st.empty()
         full_response = ''
@@ -91,11 +107,18 @@ if prompt := st.chat_input('Your message here...'):
     # Add assistant response to chat history
     st.session_state.messages.append(
         dict(
-            role=st.session_state.chat.history[-1].role,
+            # role=st.session_state.chat.history[-1].role,
+            role=model_role,
             content=st.session_state.chat.history[-1].parts[0].text,
         )
     )
     st.session_state.gemini_history = st.session_state.chat.history
     # Save to file
-    joblib.dump(st.session_state.messages, f'data/{chat_id}-st_messages')
-    joblib.dump(st.session_state.gemini_history, f'data/{chat_id}-gemini_messages')
+    joblib.dump(
+        st.session_state.messages,
+        f'data/{st.session_state.chat_id}-st_messages',
+    )
+    joblib.dump(
+        st.session_state.gemini_history,
+        f'data/{st.session_state.chat_id}-gemini_messages',
+    )
