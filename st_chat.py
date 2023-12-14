511b74417fd3a927277ca761fdefe72ad0b9a421
MrGeislinger <9027783+MrGeislinger@users.noreply.github.com>
2023-12-13 21:47:53 -0800

    Allow multiple chat histories via sidebar

--- st_chat.py
+++ st_chat.py
@@ -7,23 +7,36 @@ import google.generativeai as genai
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
+new_chat_id = f'{time.time()}'
+
+# Sidebar allows a list of past chats
+with st.sidebar:
+    st.write('# Past Chats')
+    past_chats: dict = joblib.load('past_chats.json')
+    chat_id = st.selectbox(
+        label='Pick a past chat',
+        options=[new_chat_id] + list(past_chats.keys()),
+        format_func=lambda x: past_chats.get(x, 'New Chat'),
+        placeholder='_',
+    )
+    # Save new chats after a message has been sent to AI
+        
 
 st.write('# Chat with AI')
 
 # Chat history (allows to ask multiple questions)
-if 'messages' not in st.session_state:
-    try:
-        st.session_state.messages = joblib.load('temp_st_messages')
-        st.session_state.gemini_history = joblib.load('temp_gemini_messages')
-        print('old cache')
-    except:
-        st.session_state.messages = []
-        st.session_state.gemini_history = []
-        print('new_cache made')
-    st.session_state.model = genai.GenerativeModel('gemini-pro')
-    st.session_state.chat = st.session_state.model.start_chat(
-        history=st.session_state.gemini_history,
-    )
+try:
+    st.session_state.messages = joblib.load(f'data/{chat_id}-st_messages')
+    st.session_state.gemini_history = joblib.load(f'data/{chat_id}-gemini_messages')
+    print('old cache')
+except:
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+    print('new_cache made')
+st.session_state.model = genai.GenerativeModel('gemini-pro')
+st.session_state.chat = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -33,6 +46,12 @@ for message in st.session_state.messages:
 
 # React to user input
 if prompt := st.chat_input('Your message here...'):
+    # Save this as a chat for later
+    if chat_id not in past_chats.keys():
+        # TODO: Give user a chance to name chat
+        chat_title = f'NEW'
+        past_chats[chat_id] = f'{chat_title}-{chat_id}'
+    joblib.dump(past_chats, 'past_chats.json')
     # Display user message in chat message container
     with st.chat_message('user'):
         st.markdown(prompt)
@@ -78,5 +97,5 @@ if prompt := st.chat_input('Your message here...'):
     )
     st.session_state.gemini_history = st.session_state.chat.history
     # Save to file
-    joblib.dump(st.session_state.messages, 'temp_st_messages')
-    joblib.dump(st.session_state.gemini_history, 'temp_gemini_messages')
+    joblib.dump(st.session_state.messages, f'data/{chat_id}-st_messages')
+    joblib.dump(st.session_state.gemini_history, f'data/{chat_id}-gemini_messages')
